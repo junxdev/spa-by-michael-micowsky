adf8a45605428a262a1106b25aac9d2623c2db99
jun-win-server <junxdev@gmail.com>
2020-11-11 22:22:16 +0900

    3.5.1 채팅 슬라이더를 여닫는 메서드 작성 1. 채팅 슬라이더가 움직이는 속도와 높이를 개발자가 설정할 수 있도록 작성 2. 채팅 슬라이더를 열고 닫는 단일 메서드 작성 3. 슬라이더가 동시에 열리고 닫히는 경쟁 조건(race condition) 방지 4. 슬라이더의 동작이 끝나는 시점에 호출할 수 있는 콜백 함수를 개발자가 임의로 넘겨줄 수 있도록 작성 5. 슬라이더 테스트 코드 작성

--- part-02-the-spa-client/spa/js/spa.shell.js
+++ part-02-the-spa-client/spa/js/spa.shell.js
@@ -27,11 +27,15 @@ spa.shell = (function() {
             + '</div>'
             + '<div class="spa-shell-font"></div>'
             + '<div class="spa-shell-chat"></div>'
-            + '<div class="spa-shell-modal"></div>'
+            + '<div class="spa-shell-modal"></div>',
+            chat_extend_time: 1000,
+            chat_retract_time: 300,
+            chat_extend_height: 450,
+            chat_retract_height: 15
         },
         stateMap = {container: null}, // 모듈 사이에 공유하는 동적 정보 추가
         jqueryMap = {}, // jQuery 컬렉션 객체 캐싱
-        setJqueryMap, initModule; // 선언 먼저, 나중에 대입
+        setJqueryMap, toggleChat, initModule; // 선언 먼저, 나중에 대입
     //----------- 모듈 스코프 변수 끝------------
 
     //----------- 유틸리티 메서드 시작 ------------ 페이지 엘리먼트와 상호작용하지 않는 함수만 보관
@@ -41,9 +45,51 @@ spa.shell = (function() {
     // DOM 메서드 /setJqueryMap/ 시작
     setJqueryMap = function() { // jQuery 컬렉션 객체 캐싱하는 함수. jQuery의 문서 탐색 횟수를 줄여 성능 개선
         var container = stateMap.container;
-        jqueryMap = {container: container};
+        jqueryMap = {
+            container: container,
+            chat: container.querySelector('.spa-shell-chat')
+        };
     };
     // DOM 메서드 /setJqueryMap/ 끝
+    // DOM 메서드 /toggleChat/ 시작
+    // 목적: 채팅 슬라이더영역을 열고 닫는다.
+    // 인자:
+    //  * do_extend - true면 열고, false면 닫는다.
+    //  * callback - 애니메이션 종료 시점에 callback 함수를 실행한다.
+    // 설정:
+    //  * chat_extend_time, chat_retract_time
+    //  * chat_extend_height, chat-retract_height
+    // 반환값: boolean
+    //  * true - 슬라이더 애니메이션이 실행된다.
+    //  * false - 슬라이더 애니메이션이 실행되지 않는다.
+    toggleChat = function(do_extend, callback) {
+        var
+            px_chat_ht = jqueryMap.chat.offsetHeight,
+            is_open     = px_chat_ht === configMap.chat_extend_height,
+            is_closed   = px_chat_ht === configMap.chat_retract_height,
+            is_sliding  = ! is_open && ! is_closed;
+
+        if(is_sliding) { // 슬라이드 중에는 함수를 종료
+            return false;
+        }
+
+        // 채팅 슬라이더 확장 시작
+        if(do_extend) {
+            jqueryMap.chat.style.transitionDuration = configMap.chat_extend_time + 'ms';
+            jqueryMap.chat.style.height = configMap.chat_extend_height + 'px';
+            if(callback) {callback(jqueryMap.chat);}
+            return true;
+        }
+        // 채팅 슬라이더 확장끝
+
+        // 채팅 슬라이더 축소 시작
+        jqueryMap.chat.style.transitionDuration = configMap.chat_retract_time + 'ms';
+        jqueryMap.chat.style.height = configMap.chat_retract_height + 'px';
+        if(callback) {callback(jqueryMap.chat);}
+        return true;
+        // 채팅 슬라이더 축소 끝
+    };
+    // DOM 메서드 /toggleChat/ 끝
     //----------- DOM 메서드 끝 -------------
 
     //----------- 이벤트 핸들러 시작 ------------- jQuery 이벤트 핸들러 함수 영역
@@ -52,9 +98,14 @@ spa.shell = (function() {
     //----------- public 메서드 시작 ------------- 외부로 노출하는 메서드 영역
     // public 메서드/initModule/ 시작
     initModule = function(container) { // 모듈을 초기화하는 메서드
+        // HTML을 로드한 후 jQuery 컬렉션 객체를 매핑한다.
         stateMap.container = container;
         container.innerHTML = configMap.main_html;
         setJqueryMap();
+
+        // 토글 테스트
+        setTimeout(function() {toggleChat(true);}, 3000);
+        setTimeout(function() {toggleChat(false);}, 8000);
     };
     // public 메서드/initModule/ 끝
     return {initModule: initModule}; // 공개 메서드를 맵에 넣어 반환
